c5857c583bd5c4524900ee5500d46c0eebe4a507
Tanner Evins <tanner@tannerevins.com>
2017-06-10 16:31:19 -0500

    Show table instead of a list

--- components/PatientTable.js
+++ components/PatientTable.js
@@ -2,9 +2,28 @@ import React from 'react'
 import map from 'lodash/fp/map'
 
 const PatientTable = ({ patients }) => (
-  <ul>
-    {map(p => <li key={p.account_number}>{p.first_name} {p.last_name}</li>)(patients)}
-  </ul>
+  <table>
+    <thead>
+      <tr>
+        <th>Acct. #</th>
+        <th>Last Name</th>
+        <th>First Name</th>
+        <th>Created</th>
+        <th>Updated</th>
+      </tr>
+    </thead>
+    <tbody>
+      {map(p => (
+        <tr key={p.account_number}>
+          <td>{p.account_number}</td>
+          <td>{p.last_name}</td>
+          <td>{p.first_name}</td>
+          <td>{p.created_at}</td>
+          <td>{p.udpated_at}</td>
+        </tr>
+      ))(patients)}
+    </tbody>
+  </table>
 )
 
 export default PatientTable
