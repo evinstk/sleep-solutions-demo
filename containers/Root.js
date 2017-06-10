e8cd311d3692a72dbc247eda28956ef69b6a52ad
Tanner Evins <tanner@tannerevins.com>
2017-06-10 16:17:19 -0500

    List patients

--- containers/Root.js
+++ containers/Root.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
+import map from 'lodash/fp/map'
 import { fetchPatients } from '../actions/data'
 
 class Root extends React.Component {
@@ -10,14 +11,23 @@ class Root extends React.Component {
   }
 
   render() {
+    const { patients } = this.props
+
     return (
-      <div>Redux!</div>
+      <div>
+        <h1>Patients</h1>
+        <ul>
+          {map(p => <li key={p.account_number}>{p.first_name} {p.last_name}</li>)(patients)}
+        </ul>
+      </div>
     )
   }
 }
 Root.propTypes = {
   dispatch: PropTypes.func.isRequired
 }
-Root = connect()(Root)
+Root = connect(({ entities: { patients } }) => {
+  return { patients }
+})(Root)
 
 export default Root
